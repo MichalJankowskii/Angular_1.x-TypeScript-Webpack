ee20f9c196ada8bf43c8f1996e5e15fc6ffa6c24
Michał Jankowski <jankowski.m@gmail.com>
2017-03-07 13:50:41 +0100

    Removal last external dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 import "./imports/libs";
 import "./imports/basic";
 import "./imports/styles";
-import { Bootstraper } from "angular1_typescript_decorators/Decorators";
 import { appHttpConfig } from "./app/config/httpConfig/httpConfig";
 import { provideState } from "./app/config/stateConfig/stateConfig";
 import * as angular from "angular";
-
 
 const externalModulesNames: Array<any> = [
   "ui.router",
   "ui.bootstrap",
   "ngAnimate",
   "ngSanitize",
-  "LocalStorageModule",
   "App.Header",
   "App.Footer",
   "App.Home",
@@ -25,6 +22,5 @@ const externalModulesNames: Array<any> = [
 var baseModule: angular.IModule = angular.module("App", externalModulesNames);
 baseModule.config(provideState);
 baseModule.config(appHttpConfig);
-Bootstraper.BootstrapModule(baseModule);
 angular.bootstrap(document, [baseModule.name]);
 
