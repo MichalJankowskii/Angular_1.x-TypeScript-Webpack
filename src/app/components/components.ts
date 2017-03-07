ebe8340c4fe88436b9918b4584eff53a110ad674
Michal Jankowski <mjankowski@objectivity.co.uk>
2017-03-07 11:17:03 +0100

    Removal ToDoList component

--- src/app/components/components.ts
+++ src/app/components/components.ts
@@ -1,6 +1,5 @@
 import  "./home/home";
 import  "./error/error";
-import  "./todoList/todoList";
 import  "./footer/footer";
 import  "./header/header";
 import  "./menu/menu";
--- src/app/components/todoList/models/todoItem.ts
+++
@@ -1,6 +0,0 @@
-export default class ToDoItem {
-        public title: string;
-        public description: string;
-        public priority: number;
-        public isDone: boolean;
-}
