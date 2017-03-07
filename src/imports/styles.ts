b9a7f49252d7811cc79a17b530cccbbf0726b89b
Michał Jankowski <jankowski.m@gmail.com>
2017-03-07 07:12:45 +0100

    Move styles to other folder

--- src/imports/styles.ts
+++ src/imports/styles.ts
@@ -1,4 +1,4 @@
 import "font-awesome/css/font-awesome.css";
 import "bootstrap/dist/css/bootstrap.css";
-import "../styles/screen.scss";
+import "app/assets/styles/screen.scss";
 import "animate.css";
