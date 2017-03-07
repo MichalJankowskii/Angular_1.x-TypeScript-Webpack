5149c010d1b0272a126c19fb8b4c441698eebca1
Michał Jankowski <jankowski.m@gmail.com>
2017-03-07 09:30:15 +0100

    Change page module tag

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -2,4 +2,4 @@ import "./header.scss";
 import * as angular from "angular";
 import HeaderComponent from "app/components/header/header.component";
 
-angular.module("App.Header", []).component("ingHeader", new HeaderComponent())
+angular.module("App.Header", []).component("pageHeader", new HeaderComponent())
