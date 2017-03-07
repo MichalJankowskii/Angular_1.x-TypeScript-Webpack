2cd27b8560ac3cef5480c3bd4aa29e91d8d42836
Michał Jankowski <jankowski.m@gmail.com>
2017-03-07 20:08:35 +0100

    # removal from address

--- src/app/config/stateConfig/stateConfig.ts
+++ src/app/config/stateConfig/stateConfig.ts
@@ -2,6 +2,10 @@ import {StatesBootstraper} from "StateDecorator";
 
 export function provideState($stateProvider: any, $urlRouterProvider: any, $locationProvider: any): void {
   $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false,
+  });
   StatesBootstraper.getInstance().registerStates($stateProvider);
 }
 provideState.$inject = ["$stateProvider", "$urlRouterProvider", "$locationProvider"];
